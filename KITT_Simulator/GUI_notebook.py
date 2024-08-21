550720e9fae1c86c29d5a6c7b34903c8cefdef0f
ymirwani <72574008+ymirwani@users.noreply.github.com>
2024-08-21 10:21:17 +0200

    Add Arena class simplified version

--- KITT_Simulator/GUI_notebook.py
+++ KITT_Simulator/GUI_notebook.py
@@ -19,3 +19,69 @@ class Mic:
         canvas.stroke_circle(x, y, 5)
         canvas.fill_style = 'black'
         canvas.fill_text(str(self.id), x + 7, y + 2)
+
+class Arena:
+    def __init__(self, canvas):
+        self.canvas = canvas
+        self.obstacles = [
+            [(200, 200), (260, 200), (260, 240), (230, 240)],
+            [(350, 200), (410, 200), (410, 240), (380, 240)]
+        ]
+        self.waypoints = [
+            (360, 360, 20)
+        ]
+        self.boundaries = [13, 13, 517, 517]
+        self.inner_boundaries = [13, 13, 517, 517]
+        self.outer_boundaries = [8, 8, 522, 522]
+        self.mic_positions = [
+            (12, 12, 0),
+            (517, 12, 0),
+            (12, 517, 0),
+            (517, 517, 0)
+        ]
+        self.mics = [Mic(i + 1, pos) for i, pos in enumerate(self.mic_positions)]
+        self.drawn = False  # To check if the arena is already drawn
+
+    def draw(self):
+        if not self.drawn:
+            # Draw outer boundaries
+            self.canvas.fill_style = '#808080'
+            self.canvas.fill_rect(self.outer_boundaries[0], self.outer_boundaries[1],
+                                  self.outer_boundaries[2] - self.outer_boundaries[0],
+                                  self.outer_boundaries[3] - self.outer_boundaries[1])
+            
+            # Draw inner boundaries
+            self.canvas.fill_style = 'white'
+            self.canvas.fill_rect(self.inner_boundaries[0], self.inner_boundaries[1],
+                                  self.inner_boundaries[2] - self.inner_boundaries[0],
+                                  self.inner_boundaries[3] - self.inner_boundaries[1])
+            
+            # Draw boundaries
+            self.canvas.stroke_style = 'red'
+            self.canvas.set_line_dash([4, 2])
+            self.canvas.stroke_rect(self.boundaries[0], self.boundaries[1],
+                                    self.boundaries[2] - self.boundaries[0],
+                                    self.boundaries[3] - self.boundaries[1])
+            self.canvas.set_line_dash([])  # reset line dash
+            
+            self.canvas.fill_style = 'salmon'
+            self.canvas.stroke_style = 'black'
+            for obstacle in self.obstacles:
+                self.canvas.fill_polygon(obstacle)
+                self.canvas.stroke_polygon(obstacle)
+            
+            self.canvas.fill_style = 'red'
+            for waypoint in self.waypoints:
+                x, y, radius = waypoint
+                self.canvas.fill_circle(x, y, radius)
+                self.canvas.stroke_circle(x, y, radius)
+            
+            for mic in self.mics:
+                mic.plot(self.canvas)
+
+            self.drawn = True  # Mark arena as drawn
+
+    def point_in_polygon(self, x, y, polygon):
+        point = Point(x, y)
+        poly = Polygon(polygon)
+        return point.within(poly)
