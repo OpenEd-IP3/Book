751a7a49d6b26d31ece2b9904de44913b98522fe
ymirwani <72574008+ymirwani@users.noreply.github.com>
2024-08-21 10:20:22 +0200

    Add simply Mic class to avoid discrepancies

--- KITT_Simulator/GUI_notebook.py
+++ KITT_Simulator/GUI_notebook.py
@@ -5,3 +5,17 @@ from ipycanvas import Canvas
 from shapely.geometry import Point, Polygon
 import ipywidgets as widgets
 from IPython.display import display
+
+class Mic:
+    def __init__(self, id, position):
+        self.id = id
+        self.position = position
+
+    def plot(self, canvas):
+        x, y, _ = self.position
+        canvas.fill_style = 'blue'
+        canvas.fill_circle(x, y, 5)
+        canvas.stroke_style = 'black'
+        canvas.stroke_circle(x, y, 5)
+        canvas.fill_style = 'black'
+        canvas.fill_text(str(self.id), x + 7, y + 2)
